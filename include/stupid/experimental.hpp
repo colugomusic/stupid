e43ad97e941af4cb1e9b667308a50cc0bb9729de
Colugo <68328892+colugomusic@users.noreply.github.com>
2021-10-14 17:00:11 +0100

    More experimental stuff

--- include/stupid/experimental.hpp
+++ include/stupid/experimental.hpp
@@ -42,7 +42,7 @@ public:
 
     Immutable() = default;
 
-    Immutable(Record* record)
+    Immutable(Record<T>* record)
         : record_(record)
     {
         if (record_) record_->ref();
@@ -53,13 +53,13 @@ public:
         if (record_) record_->unref();
     }
 
-    Immutable(const Immutable& rhs)
+    Immutable(const Immutable<T>& rhs)
         : record_(rhs.record_)
     {
         if (record_) record_->ref();
     }
 
-    Immutable& operator=(const Immutable& rhs)
+    Immutable<T>& operator=(const Immutable<T>& rhs)
     {
         record_ = rhs.record_;
 
@@ -80,7 +80,7 @@ public:
 
 private:
 
-    Record* record_ { nullptr };
+    Record<T>* record_ { nullptr };
 };
 
 template <class T>
@@ -145,16 +145,22 @@ class Read
 
 public:
 
-    Immutable<T> get()
+    void update()
     {
-		retrieved_ = object_->get();
+        if (object_->pending())
+        {
+		    object_->get();
+        }
+    }
 
+    Immutable<T> get() const
+    {
 		return retrieved_;
     }
 
-    const Immutable<T> get() const
+    const T& get_data() const
     {
-		return retrieved_;
+        return *retrieved_;
     }
 
 private:
@@ -178,6 +184,9 @@ public:
 
     T* copy() { return object_->copy(); }
     void commit(T* data) { object_->commit(data); }
+
+    template <class ... Args>
+    void commit_new(Args... args) { object_->commit(new T(args...)); }
 
 	Immutable<T> get() { return Immutable<T> { object_->last_written_record_.load() }; }
 	const Immutable<T> get() const { return Immutable<T> { object_->last_written_record_.load() }; }
@@ -212,11 +221,15 @@ public:
     Write<T>& write() { return write_; }
     const Write<T>& write() const { return write_; }
 
+    bool pending() const { return pending_record_.load(); }
+
 private:
 
     Immutable<T> get()
     {
-        return Immutable<T> { last_written_record_.load() };
+        assert(pending());
+
+        return Immutable<T> { pending_record_.exchange(nullptr) };
     }
 
     T* copy()
@@ -232,7 +245,9 @@ private:
     {
         const auto record = book_.make_record(data);
 
+        pending_record_.store(record);
         last_written_record_.store(record);
+        pending_ref_ = Immutable<T> { record };
         last_written_ = Immutable<T> { record };
 
         book_.collect();
@@ -241,12 +256,116 @@ private:
     Read<T> read_;
     Write<T> write_;
 
-    Book book_;
+    Book<T> book_;
 
+    std::atomic<Record<T>*> pending_record_ { nullptr };
     std::atomic<Record<T>*> last_written_record_ { nullptr };
 
     // Keep at least one reference until overwritten
+    Immutable<T> pending_ref_;
     Immutable<T> last_written_ref_;
+};
+
+class SyncSignal
+{
+public:
+
+	std::uint32_t get_value() const { return value_; }
+	void operator()() { value_++; }
+
+private:
+
+	std::uint32_t value_ = 0;
+};
+
+template <class T, class SignalType = SyncSignal>
+class SignalSyncObject
+{
+
+public:
+
+    SignalSyncObject(const SignalType& signal)
+        : signal_(&signal)
+    {
+    }
+    
+    const T& get_data()
+    {
+        if (!retrieved_) update();
+
+        return *retrieved_;
+    }
+
+    Write<T>& write() { return object_.write(); }
+    const Write<T>& write() const { return object_.write(); }
+
+private:
+
+    void update()
+    {
+        const auto signal_value = signal_->get_value();
+
+        if (signal_value > slot_value_ && object_.pending())
+        {
+            object_.read().update();
+            retrieved_ = object_.read().get();
+        }
+
+        slot_value_ = signal_value;
+    }
+    
+    Object<T> object_;
+    SignalType* signal_;
+    std::uint32_t slot_value_ { 0 };
+    Immutable<T> retrieved_;
+};
+
+template <class T, class SignalType = SyncSignal>
+class SignalSyncObjectPair
+{
+public:
+
+	SignalSyncObjectPair(const SignalType& signal)
+		: signal_(&signal)
+	{
+	}
+
+    // If there's data pending, store it in [0|1].
+	void update(int idx)
+	{
+        const auto signal_value = signal_->get_value();
+        
+        if (signal_value > slot_value_ && object_.pending())
+		{
+            retrieved_[idx] = object_.read().update();
+            recent_[idx] = retrieved_[idx];
+		}
+
+        slot_value_ = signal_value;
+	}
+
+    // Get the current data for [0|1].
+    const T& get_data(int idx)
+    {
+        if (retrieved_[idx]) return *retrieved_[idx];
+        if (retrieved_[flip(idx)]) return *retrieved_[flip(idx)];
+
+        update(idx);
+
+        return *retrieved_[idx];
+    }
+
+    Write<T>& write() { return object_.write(); }
+    const Write<T>& write() const { return object_.write(); }
+
+private:
+
+	static int flip(int x) { return 1 - x; }
+
+    Object<T> object_;
+    SignalType* signal_;
+    std::uint32_t slot_value_ { 0 };
+    std::array<Immutable<T>, 2> retrieved_;
 };
 
 } // experimental
